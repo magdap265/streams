df9406395c832e29cd734e75f8cac830fdf7bbf0
Magda <magdap.265@gmail.com>
2019-10-16 11:32:28 +0200

    Aplikacja jest tworzona na podstawie kursu 'Modern React with Redux'. Docelowo będzie to prosty portal ze streamami. Użytkownik będzie miał możliwość zalogować sie za pośrednictwem google, następnie będzie mógł edytować lub usunąć swoje pliki jak rownież dodać nowe. Będzie tez możliwośc oglądania streamów udostępnionych przez innych uzytkowników. Na ten moment można się luż zalogować oraz wylogować za pośrednictwem swojego gonta google.

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+    state={ isSignedIn: null }
+
+componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+        window.gapi.client.init({
+            clientId: '830217806314-au1451ggc94s751brrg23cfd0si080l3.apps.googleusercontent.com',
+            scope: 'email'
+        }).then(() => {
+            this.auth = window.gapi.auth2.getAuthInstance();
+            this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+            this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+}
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    };
+
+    onSignIn = () => {
+        this.auth.signIn();
+    };
+
+    onSignOut = () => {
+        this.auth.signOut();        
+    };
+
+    renderAuthButton(){
+        if (this.state.isSignedIn === null) {
+            return <div>I don't know if you re signed in.</div>;
+        } else if (this.state.isSignedIn){
+            return (( 
+                <button 
+                    className="ui red google button"
+                    onClick={this.onSignOut}
+                >
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            ))
+        } else{
+            return (( 
+                <button 
+                    className="ui red google button"
+                    onClick={this.onSignIn}
+                >
+                    <i className="google icon" />
+                    Sign In with google
+                </button>
+            ))      
+        }
+    }
+
+
+    render() {
+        return(
+            <div>{this.renderAuthButton()}</div>
+        );
+    };
+};
+
+export default GoogleAuth;
